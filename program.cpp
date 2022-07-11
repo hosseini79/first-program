68554a8142a798d2aab6000bd4cdc82fc60ae21b
hosseini79 <72670319+hosseini79@users.noreply.github.com>
2022-07-11 09:50:39 +0430

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+float avg(float num1,float num2);
+float sum(float num1,float num2);
+int main()
+{
+	float a,sum,avg;
+	for(i=0;i<13;i++)
+	{
+		cin >> a;
+		sum= sum (num1,num2);
+		avg= avg (num1,num2);
+	}
+	if((sum/13)>=12)
+	{
+		cout <<"pass";
+	}
+	else((sum/13)<=12)
+	{
+		cout <<"fail";
+	}
+	cout <<avg;
+	return0;
+}
+float sum (float num1,float num2)
+{
+	float sum;
+	sum=sum+a;
+	return sum;
+}
+float avg (float num1,float num2)
+{
+	float avg;
+	avg=sum/13;
+	return avg;
+}
